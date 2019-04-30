310f54ce5a8106815a6a127d35001df360f09ba8
Laurent Pollez <laurent.pollez@gmail.com>
2019-04-30 17:22:37 +0200

    my first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Restaurant`,
+    description: `Site pour un restaurant créé avec Gatsby`,
+    author: `Laurent`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
