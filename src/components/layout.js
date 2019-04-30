11c7536c8cc9597219791536534d48b8a46b3196
Laurent Pollez <laurent.pollez@gmail.com>
2019-04-30 18:00:11 +0200

    setup Navbar & styled-components

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,12 +8,26 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import "./bootstrap.min.css"
-import "./layout.css"
+// import "./bootstrap.min.css"
+// import "./layout.css"
+import { createGlobalStyle } from "styled-components"
 
 const Layout = ({ children }) => {
-  return <React.Fragment>{children}</React.Fragment>
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      {children}
+    </React.Fragment>
+  )
 }
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
