79519c6e3925fce42001da7b96592db1e68dd81b
Laurent Pollez <laurent.pollez@gmail.com>
2019-05-03 20:59:46 +0200

    Ajout Contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
     title: `Restaurant`,
@@ -43,6 +46,14 @@ module.exports = {
             variants: [`400`],
           },
         ],
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
